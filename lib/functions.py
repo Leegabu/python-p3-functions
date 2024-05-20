8f7589236f68f36f2c94ae117d49d9da6a342ed1
Lee <leeirungu11313@gmail.com>
2024-05-21 01:23:29 +0300

    Python

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,25 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
 def greet(name):
     pass
-
+    print(f"Hello, {name}!")
+greet("Giudo")
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+greet_with_default("")
 
 def add(num1, num2):
     pass
+    return num1 + num2
+ans = add(45,55)
+print(ans)
 
 def halve(number):
     pass
+    return number / 2
+num = halve(99.0)
+print(num)
